0e4fb76982dfc2c6ecbb0b8e861f48987c471cd4
rohit-accolite <rohit.verma@accolitedigital.com>
2021-01-16 19:45:41 +0530

    ang

--- Angular1&2-Moring&Afternoon/rohitAngularAssignment/src/app/navigate/todoModel.ts
+++
@@ -1,7 +0,0 @@
-export class todo {
-    userId: number = 0;
-    id: number = 0 ;
-    title: String = "";
-    completed: boolean = false;
-    display: boolean = false;
-}
--- Angular1&2-Moring&Afternoon/rohitAngularAssignment/src/app/session/model.ts
+++
@@ -1,9 +0,0 @@
-export class model {
-    name: String = "";
-    trainer: String = "";
-    date: String = "";
-    details: String = "";
-    isVisible: Boolean = true;
-    showDetails: Boolean = false;
-    edit: Boolean = false;
-}
